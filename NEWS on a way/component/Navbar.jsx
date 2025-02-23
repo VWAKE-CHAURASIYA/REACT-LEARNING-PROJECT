d335f2439361b550f6330ad91a8e6f91577b202a
Vivek Chaurasiya <108581722+VWAKE-CHAURASIYA@users.noreply.github.com>
2025-02-23 23:06:56 +0530

    Create Navbar.jsx

---
+++ NEWS on a way/component/Navbar.jsx
@@ -0,0 +1,112 @@
+const Navbar = ({ setCategory }) => {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg bg-body-tertiary"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <span className="badge bg-danger text-dark fs-5">
+              NEWS ON A WAY
+            </span>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCategory("technology");
+                  }}
+                >
+                  Technology
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCategory("business");
+                  }}
+                >
+                  Business
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCategory("science");
+                  }}
+                >
+                  Science
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCategory("sports");
+                  }}
+                >
+                  Sports
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCategory("health");
+                  }}
+                >
+                  Health
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCategory("entertainment");
+                  }}
+                >
+                  Entertainment
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
